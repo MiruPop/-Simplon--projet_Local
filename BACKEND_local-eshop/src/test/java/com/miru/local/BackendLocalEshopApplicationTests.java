fc34bd7b51ef84000bc5eea340262d80df1b2177
Miruna Popescu <miru.popescu@yahoo.fr>
2022-05-29 17:44:35 +0200

    testing - part1

--- BACKEND_local-eshop/src/test/java/com/miru/local/BackendLocalEshopApplicationTests.java
+++ BACKEND_local-eshop/src/test/java/com/miru/local/BackendLocalEshopApplicationTests.java
@@ -2,8 +2,14 @@ package com.miru.local;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration
 class BackendLocalEshopApplicationTests {
 
 	@Test
